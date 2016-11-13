84e2c66c0d91aed62f9fbe4f14911dd5a6c78867
hsahluwalia9 <harsimar.ahluwalia@gmail.com>
2016-11-13 12:40:57 -0800

    Add files via upload

---
+++ EmployeeClass/EmployeeClass/Employee.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeClass
+{
+    class Employee: INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        private string _idNumber;
+        public string IdNumber
+        {
+            get { return _idNumber; }
+            set { _idNumber = value; OnPropertyChanged(); }
+        }
+
+        private string _department;
+        public string Department
+        {
+            get { return _department; }
+            set { _department = value; OnPropertyChanged(); }
+        }
+
+        private string _position;
+        public string Position
+        {
+            get { return _position; }
+            set { _position = value; OnPropertyChanged(); }
+        }
+        
+        //Overload constructor 1
+        public Employee(string name, string idNumber, string department, string position)
+        {
+            name = _name;
+            idNumber = _idNumber;
+            department = _department;
+            position = _position;
+        }
+
+        //Overload constructor 2
+        public Employee(string name, string idNumber)
+        {
+            name = _name;
+            idNumber = _idNumber;
+            Department = "";
+            Position = "";
+        }
+
+        //Overload constructor 3
+        public Employee()
+        {
+            Name = "";
+            IdNumber = "";
+            Department = "";
+            Position = "";
+        }
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string caller = null)
+        {
+
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+        #endregion
+    }
+}
---
+++ EmployeeClass/EmployeeClass/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmployeeClass
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Employee vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new Employee();
+            DataContext = vm;
+        }
+
+        private void BtnDisplay_Click(object sender, RoutedEventArgs e)
+        {
+            //Employee 1 object
+            Employee Employee1 = new Employee(vm.Name, vm.IdNumber, vm.Position, vm.Department);
+         
+        }
+    }
+}
